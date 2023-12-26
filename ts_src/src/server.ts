9cf0c21109d60bf247c1ddcded638935d4aca21f
Adrian Gamarra <adriangamarra2018@gmail.com>
2023-12-26 00:06:35 -0800

    ts env

---
+++ ts_src/src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, Express in TypeScript!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
